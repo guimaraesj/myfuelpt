5417b9478d7be151064380a4c3fe4fa5bc3d6fb3
Jorge Guimaraes <jdguimaraes@hotmail.com>
2022-11-28 23:33:12 +0000

    stations, vechicles & controllers gen + models relations

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,6 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
+  # when we have the API we can select Simples, Especial & B15 are Gasoleo
+  has_many :fuel_types
+  validates :real_consumption, presence: true
 end
