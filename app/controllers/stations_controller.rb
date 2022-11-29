64043d0240a1cf95bc8cb0af0c430cb9067e5665
Francisco Patricio <fpatricio1@proton.me>
2022-11-29 13:27:36 +0000

    map added

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,10 +1,16 @@
 class StationsController < ApplicationController
   def index
     @stations = Station.all
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @stations.geocoded.map do |station|
+      {
+        lat: station.latitude,
+        lng: station.longitude
+      }
+    end
   end
 
   def show
-    authorize @product # pundit authorization to anyone
+    # authorize @product # pundit authorization to anyone
   end
-
 end
---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="map"
+export default class extends Controller {
+  static values = { apiKey: String }
+
+  connect() {
+    mapboxgl.accessToken = this.apiKeyValue
+    const map = new mapboxgl.Map({
+      container: this.element,
+      style: 'mapbox://styles/mapbox/streets-v10' // style URL
+    });
+  }
+}
