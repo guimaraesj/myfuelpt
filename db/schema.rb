a5d83b8a2db5aae52f5a55b4bc1645b5b4ef610d
Jorge Guimaraes <jdguimaraes@hotmail.com>
2022-11-29 13:07:40 +0000

    seeds incomplete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_122443) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_123717) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_122443) do
     t.string "address"
     t.float "lat"
     t.float "lng"
-    t.string "type"
     t.string "brand"
     t.text "services"
     t.text "schedule"
@@ -39,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_122443) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "last_comunication_date"
+    t.string "station_type"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,11 @@ Station.create(
   address: "Rua Brito Capelo, 951",
   lat: 41.17947397622519,
   lng: -8.689222942994473,
-  type: "passageiros e pesados",
+  station_type: "passageiros e pesados",
   brand: "Galp",
-  services: ["Lavagem Auto", "WC", "ATM", "Loja Tangerina", "Visa", "Multibanco"],
-  schedule: "2ª: 9-20, 3ª 9-20",
-  fuel_types: ["gasoleo_simples", "gasoleo_especial", "gasolina_especial_95", "gasolina_simples_95", "gasolina_especial_98"],
+  services: ["Lavagem Auto", "WC", "ATM", "Loja Tangerina", "Visa", "Multibanco"].to_json,
+  schedule: "2ª: 9-20, 3ª: 9-20",
+  fuel_types: ["gasoleo_simples", "gasoleo_especial", "gasolina_especial_95", "gasolina_simples_95", "gasolina_especial_98"].to_json,
   price: {
     gasoleo_simples: 1.7890,
     gasoleo_especial: 1.8390,
@@ -36,11 +36,11 @@ Station.create(
   address: "Avenida Arquiteto Fernando Távora, 718",
   lat: 41.21025660368514,
   lng: -8.68271578161743,
-  type: "passageiros e pesados",
+  station_type: "passageiros e pesados",
   brand: "Prio",
-  services: ["Lavagem Auto", "WC", "ATM", "Loja Tangerina", "Visa", "Multibanco"],
+  services: ["Lavagem Auto", "WC", "ATM", "Loja Tangerina", "Visa", "Multibanco"].to_json,
   schedule: "2ª: 9-20, 3ª 9-20",
-  fuel_types: ["gasoleo_simples", "gasoleo_especial", "gasolina_especial_95", "gasolina_simples_95", "biodiesel_b15"],
+  fuel_types: ["gasoleo_simples", "gasoleo_especial", "gasolina_especial_95", "gasolina_simples_95", "biodiesel_b15"].to_json,
   price: {
     gasoleo_simples: 1.6890,
     gasoleo_especial: 1.6390,
@@ -57,11 +57,11 @@ Station.create(
   address: "Avenida Arquiteto Fernando Távora, 718",
   lat: 41.21025660368514,
   lng: -8.68271578161743,
-  type: "passageiros e pesados",
+  station_type: "passageiros e pesados",
   brand: "Prio",
-  services: ["Lavagem Auto", "WC", "ATM", "Loja Tangerina", "Visa", "Multibanco"],
+  services: ["Lavagem Auto", "WC", "ATM", "Loja Tangerina", "Visa", "Multibanco"].to_json,
   schedule: "2ª: 9-20, 3ª 9-20",
-  fuel_types: ["gasoleo_simples", "gasoleo_especial", "gasolina_especial_95", "gasolina_simples_95", "biodiesel_b15"],
+  fuel_types: ["gasoleo_simples", "gasoleo_especial", "gasolina_especial_95", "gasolina_simples_95", "biodiesel_b15"].to_json,
   price: {
     gasoleo_simples: 1.6890,
     gasoleo_especial: 1.6390,
@@ -78,11 +78,11 @@ Station.create(
   address: "Rua de São Miguel, São Miguel de Baixo",
   lat: 41.43914045640935,
   lng: -8.307753971534092,
-  type: "passageiros e pesados",
+  station_type: "passageiros e pesados",
   brand: "Shell",
-  services: ["Acessibilidade", "Ar-Agua", "Gas 9kg", "Loja Conveniencia", "Lubrificantes", "Wifi-gratuito"],
+  services: ["Acessibilidade", "Ar-Agua", "Gas 9kg", "Loja Conveniencia", "Lubrificantes", "Wifi-gratuito"].to_json,
   schedule: "2ª: 7-23, 3ª 7-23",
-  fuel_types: ["gasoleo_simples", "gasoleo_especial", "gasolina_especial_95", "gasolina_simples_95"],
+  fuel_types: ["gasoleo_simples", "gasoleo_especial", "gasolina_especial_95", "gasolina_simples_95"].to_json,
   price: {
     gasoleo_simples: 1.8890,
     gasoleo_especial: 1.8390,
@@ -98,11 +98,11 @@ Station.create(
   address: "Largo de São Brás, Guimarães",
   lat: 41.44146733953273,
   lng: -8.289682350412324,
-  type: "passageiros e pesados",
+  station_type: "passageiros e pesados",
   brand: "Prio",
-  services: ["Acessibilidade", "WC", "Atendimento", "Ar-Agua", "Loja Conveniencia", "Lubrificantes"],
+  services: ["Acessibilidade", "WC", "Atendimento", "Ar-Agua", "Loja Conveniencia", "Lubrificantes"].to_json,
   schedule: "2ª: 7-23, 3ª 7-23",
-  fuel_types: ["gasoleo_simples", "gasoleo_especial", "gasolina_especial_95", "gasolina_simples_95"],
+  fuel_types: ["gasoleo_simples", "gasoleo_especial", "gasolina_especial_95", "gasolina_simples_95"].to_json,
   price: {
     gasoleo_simples: 1.6890,
     gasoleo_especial: 1.6190,
@@ -118,11 +118,11 @@ Station.create(
   address: "Rua de Alfredo Cunha 546, 4450-022 Matosinhos",
   lat: 41.186565030998764,
   lng: -8.677973147679213,
-  type: "passageiros e pesados",
+  station_type: "passageiros e pesados",
   brand: "Cepsa",
-  services: ["Loja", "Lavagem Auto", "Ar-Agua"],
+  services: ["Loja", "Lavagem Auto", "Ar-Agua"].to_json,
   schedule: "Seg-Dom 00:00 - 24:00",
-  fuel_types: ["gasoleo_simples", "gasoleo_especial", "gasolina_especial_95", "gasolina_simples_95"],
+  fuel_types: ["gasoleo_simples", "gasoleo_especial", "gasolina_especial_95", "gasolina_simples_95"].to_json,
   price: {
     gasoleo_simples: 1.6890,
     gasoleo_especial: 1.6190,
@@ -138,11 +138,11 @@ Station.create(
   address: "Rua Santa Eufémia, 174, 4485-060 Canidelo",
   lat: 41.312712068640856,
   lng: -8.634300222501045,
-  type: "passageiros e pesados",
+  station_type: "passageiros e pesados",
   brand: "Q8",
-  services: ["Loja", "Lavagem Auto", "Ar-Agua"],
+  services: ["Loja", "Lavagem Auto", "Ar-Agua"].to_json,
   schedule: "Seg-Dom 07:00 - 21:00",
-  fuel_types: ["gasoleo_simples", "gasoleo_especial", "gasóleo colorido", "gasolina_especial_95", "gasolina_simples_95", "gasolina_especial_98", "gasolina_mistura"],
+  fuel_types: ["gasoleo_simples", "gasoleo_especial", "gasóleo colorido", "gasolina_especial_95", "gasolina_simples_95", "gasolina_especial_98", "gasolina_mistura"].to_json,
   price: {
     gasoleo_simples: 1.6290,
     gasoleo_especial: 1.6590,
@@ -179,37 +179,37 @@ puts "Creating Vehicles..."
 Vehicle.create(
   brand: "Kia", model: "Rio",
   real_consumption: 4.5,
-  fuel_type: "gasóleo",
+  #fuel_type: "gasóleo",
   user: User.find_by(first_name: 'Jorge')
 )
 Vehicle.create(
   brand: "Honda", model: "PCX",
   real_consumption: 2.8,
-  fuel_type: "gasolina",
+  #fuel_type: "gasolina",
   user: User.find_by(first_name: 'Jorge')
 )
 Vehicle.create(
   brand: "Bmw", model: "320",
   real_consumption: 6,
-  fuel_type: "gasóleo",
+  #fuel_type: "gasóleo",
   user: User.find_by(first_name: 'Francisco')
 )
 Vehicle.create(
   brand: "Bmw", model: "750",
   real_consumption: 9,
-  fuel_type: "gasóleo",
+  #fuel_type: "gasóleo",
   user: User.find_by(first_name: 'Francisco')
 )
 Vehicle.create(
   brand: "Mercedes", model: "250",
   real_consumption: 7,
-  fuel_type: "gasolina",
+  #fuel_type: "gasolina",
   user: User.find_by(first_name: 'Manuel')
 )
 Vehicle.create(
   brand: "Toyota", model: "Prius",
   real_consumption: 5,
-  fuel_type: "gasolina",
+  #fuel_type: "gasolina",
   user: User.find_by(first_name: 'Manuel')
 )
 puts "Created Vehicles."
