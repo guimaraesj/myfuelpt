06a1690afb4901e564fb3f697e56524413c13043
manuel esteves <mj.esteves92@gmail.com>
2022-11-29 13:01:56 +0000

    not working

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -2,4 +2,17 @@ class StationsController < ApplicationController
   def index
     @stations = Station.all
   end
+
+  def near
+    # @stations = Station.near
+    # @markers = @stations.geocoded.map do |station|
+    # {
+    #   lat: station.latitude,
+    #   lng: station.longitude
+    # }
+    @request = request
+    @latitude = request.location.latitude
+    @longitude = request.location.longitude
+    raise
+  end
 end
