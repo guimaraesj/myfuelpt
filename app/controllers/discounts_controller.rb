9bc7976895bd216b9ec0563a64852f81b7c3db87
Francisco Patricio <fpatricio1@proton.me>
2022-11-30 15:21:49 +0000

    edit discounts fixed

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,4 +1,5 @@
 class DiscountsController < ApplicationController
+  before_action :set_product, only: %i[edit update destroy]
 
   def index
     @discounts = Discount.all
@@ -21,21 +22,25 @@ class DiscountsController < ApplicationController
   end
 
   def edit
-    @discount = Discount.find(params[:product_id])
+    #@discount = Discount.find(params[:product_id])
     #authorize @discounts # pundit authorization to what is defined in rents policy
   end
 
   def update
     # authorize @discounts # pundit authorization to what is defined in rents policy
-    @discount = Discount.find(params[:product_id])
+    #@discount = Discount.find(params[:product_id])
     @discount.update(discounts_params)
     #@discounts.product = @discounts
-    redirect_to discounts_path(@discounts)
+    if @discount.save
+      redirect_to discounts_path(@discount)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
     # authorize @discounts # pundit authorization to what is defined in rents policy
-    @discount = Discount.find(params[:id])
+    #@discount = Discount.find(params[:id])
     @discount.destroy
     redirect_to discounts_path, status: :see_other
   end
@@ -43,7 +48,11 @@ class DiscountsController < ApplicationController
   private
 
   def discounts_params
-    params.require(:discounts).permit(:brand, :quantity, :min_fuel_l, :max_fuel_l, :expiry_date)
+    params.require(:discount).permit(:brand, :quantity, :min_fuel_l, :max_fuel_l, :expiry_date)
+  end
+
+  def set_product
+    @discount = Discount.find(params[:id])
   end
 
 end
