78472cad155bc24412b4dc55b2cc7878e6cf2086
Francisco Patricio <fpatricio1@proton.me>
2022-11-30 16:16:49 +0000

    navbar done

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -5,45 +5,45 @@ class DiscountsController < ApplicationController
     @discounts = Discount.all
   end
 
-  def new
-    @discount = Discount.new
-    # authorize @discounts # pundit authorization to anyone
-  end
+  # def new
+  #   @discount = Discount.new
+  #   # authorize @discounts # pundit authorization to anyone
+  # end
 
-  def create
-    @discount = Discount.new(discounts_params)
-    # @product.user = current_user
-    # authorize @product # pundit authorization to anyone
-    if @discount.save
-      redirect_to discounts_path(@discount)
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   @discount = Discount.new(discounts_params)
+  #   # @product.user = current_user
+  #   # authorize @product # pundit authorization to anyone
+  #   if @discount.save
+  #     redirect_to discounts_path(@discount)
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
 
-  def edit
-    #@discount = Discount.find(params[:product_id])
-    #authorize @discounts # pundit authorization to what is defined in rents policy
-  end
+  # def edit
+  #   #@discount = Discount.find(params[:product_id])
+  #   #authorize @discounts # pundit authorization to what is defined in rents policy
+  # end
 
-  def update
-    # authorize @discounts # pundit authorization to what is defined in rents policy
-    #@discount = Discount.find(params[:product_id])
-    @discount.update(discounts_params)
-    #@discounts.product = @discounts
-    if @discount.save
-      redirect_to discounts_path(@discount)
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   # authorize @discounts # pundit authorization to what is defined in rents policy
+  #   #@discount = Discount.find(params[:product_id])
+  #   @discount.update(discounts_params)
+  #   #@discounts.product = @discounts
+  #   if @discount.save
+  #     redirect_to discounts_path(@discount)
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
 
-  def destroy
-    # authorize @discounts # pundit authorization to what is defined in rents policy
-    #@discount = Discount.find(params[:id])
-    @discount.destroy
-    redirect_to discounts_path, status: :see_other
-  end
+  # def destroy
+  #   # authorize @discounts # pundit authorization to what is defined in rents policy
+  #   #@discount = Discount.find(params[:id])
+  #   @discount.destroy
+  #   redirect_to discounts_path, status: :see_other
+  # end
 
   private
 
