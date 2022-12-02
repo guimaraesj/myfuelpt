21ca2039be13b08368b3771c15ee08cf4206f51d
Jorge Guimaraes <jdguimaraes@hotmail.com>
2022-12-02 12:27:32 +0000

    added selectable pick for stations fuel types

--- app/controllers/stations_fuel_types_controller.rb
+++ app/controllers/stations_fuel_types_controller.rb
@@ -5,8 +5,13 @@ class StationsFuelTypesController < ApplicationController
     #after_action :verify_policy_scoped, only: %i[index], unless: :skip_pundit?
 
   def index
-    @fuel_type = FuelType.find(params[:fuel_type_id])
-    #@fuel_type = FuelType.all
-    @stations_fuel_types = StationsFuelType.where(fuel_type: params[:fuel_type_id])
+    # @fuel_type = FuelType.find(params[:fuel_type_id])
+    @fuel_types = FuelType.all
+    if params[:fuel_type_id].present?
+      @fuel_type = FuelType.find(params[:fuel_type_id])
+    else
+      @fuel_type = FuelType.find_by(name: "Gasóleo Simples")
+    end
+    @stations_fuel_types = StationsFuelType.where(fuel_type: @fuel_type)
   end
 end
