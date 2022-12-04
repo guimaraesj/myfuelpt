8250bb6d8c9138fe3463d94d00985274225ec8fe
Jorge Guimaraes <jdguimaraes@hotmail.com>
2022-12-04 23:12:29 +0000

    add extra info for price

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -12,3 +12,6 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import TooltipController from "./tooltip_controller"
+application.register("tooltip", TooltipController)
