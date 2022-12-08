c7e9e3fcc326b19a6f0fd073f5cb3caff00f7ad9
Francisco Patricio <fpatricio1@proton.me>
2022-12-08 15:28:28 +0000

    btn default

--- app/javascript/controllers/stations_order_controller.js
+++ app/javascript/controllers/stations_order_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="stations-order"
 export default class extends Controller {
-  static targets = ["sortbyprice", "sortbydistance"]
+  static targets = ["sortbyprice", "sortbydistance", "pricebutton", "distancebutton"]
 
   connect() {
     //confirmar se a informação do click chega ao console log
@@ -14,12 +14,15 @@ export default class extends Controller {
   orderByPrice() {
     this.sortbydistanceTarget.classList.add("d-none")
     this.sortbypriceTarget.classList.remove("d-none")
-    //this.sortbypriceTarget.classList.add("btn-flat:hover")
+    this.pricebuttonTarget.classList.add("active")
+    this.distancebuttonTarget.classList.remove("active")
   }
 
   orderByDistance() {
     this.sortbydistanceTarget.classList.remove("d-none")
     this.sortbypriceTarget.classList.add("d-none")
+    this.pricebuttonTarget.classList.remove("active")
+    this.distancebuttonTarget.classList.add("active")
 
   }
 }
